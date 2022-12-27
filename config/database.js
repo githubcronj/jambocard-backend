7eec1b7337e6be7df65652e62c31591072a39e81
Devashish514 <devagosw193@gmail.com>
2022-12-27 15:41:53 +0530

    docker/implement

--- config/database.js
+++ config/database.js
@@ -1,17 +1,13 @@
   // strapi-api/config/database.js
   module.exports = ({ env }) => ({
     connection: {
-      client: 'postgres',
+      client: env('DATABASE_CLIENT','postgres'),
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'strapiDB'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-project'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD',"test123"),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-        },
+        database: env('DATABASE_DATABASE', 'strapi-project'),
+        user: env('DATABASE_USER', 'postgres'),
+        password: env('DATABASE_PASSWORD',"test123")
       },
       debug: false,
     },
